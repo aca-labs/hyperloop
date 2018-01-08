42aba5c2bc73fe3cce98d166e1e907b59d2d863b
Stephen von Takach <steve@cotag.me>
2018-01-08 14:48:39 +1100

    (active-model:validation) init commit
    based on https://github.com/kemalyst/kemalyst-validators

---
+++ active-model/src/active-model/error.cr
@@ -0,0 +1,14 @@
+class ActiveModel::Error
+  property field, message
+
+  def initialize(@field : Symbol, @message : String)
+  end
+
+  def to_s
+    if @field == :base
+      @message
+    else
+      "#{@field.to_s.capitalize} #{message}"
+    end
+  end
+end
